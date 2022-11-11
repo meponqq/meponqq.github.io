14fd69bd52deaeb545d7a44f7555824905cc5ebb
meponqq <63166405+meponqq@users.noreply.github.com>
2022-11-12 01:45:27 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="ru">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta property="og:locale" content="ru_RU"/>
+		<meta property="og:site_name" content="Стример на Twitch"/>
+		<meta property="og:title" content="Mepon" />
+		<meta property="og:type" content="article"/>
+		<meta property="og:url" content="http://mepon.fun" />
+		<meta property="og:image" content="http://mepon.fun/assets/img/icon.png" />
+		<meta property="og:description" content="Оффициальный сайт Twitch стримера Mepon, со всеми основными ссылками." />
+		<meta name="theme-color" content="#7c00db">
+		<title>Mepon | Twitch streamer</title>
+		<link rel="icon" href="assets/img/icon.png">
+		<link rel="stylesheet" href="assets/css/style.css">
+		<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<script src="assets/js/jquery-3.3.1.min.js"></script>
+	</head>
+	<body class="body-main">
+		<div class="container mt-5">
+			<div class="title">
+				<h1><a style="color: #ff0000;" id='online'>◯</a> Mepon channel</h1>
+				<script>
+					var a = ["#d6ac02", "#ff0000", "#00e6ff", "#000dff"];
+					setInterval(function() {
+						document.getElementById('online').style.color = a[Math.floor(Math.random() * Math.floor(4))];
+					},360);
+
+				</script>
+			</div>
+		</div>
+
+			<div class="container text-center w-50 p-4 rounded mt-5">
+				<iframe
+					src="https://player.twitch.tv/?channel=meponchannel&parent=localhost&muted=true"
+					height="500"
+					width="800"
+					frameborder="0"
+					allowfullscreen="true">
+				</iframe>
+			</div>
+
+			<div class="links">
+				<div class="vk">
+					<a href="https://vk.com/mepon" target="_blink">
+						<span class="fab fa-vk"></span><br>
+						<small>VK</small>
+					</a>
+				</div>
+				<div class="discord">
+					<a href="https://discord.gg">
+						<span class="fab fa-discord"></span><br>
+						<small>Discord</small>
+					</a>
+				</div>
+				<div class="donate">
+					<a href="https://donate.stream/mepondonatik">
+						<span class="fas fa-coins"></span><br>
+						<small>Донат</small>
+					</a>
+				</div>
+				<div class="steam">
+					<a href="https://steamcommunity.com/id/mepon">
+						<span class="fab fa-steam"></span><br>
+						<small>Steam</small>
+					</a>
+				</div>
+				<div class="youtube">
+					<a href="https://www.youtube.com/channel/UCTt4todzqkn3vOMFMkHa3Wg">
+						<span class="fab fa-youtube"></span><br>
+						<small>YouTube</small>
+					</a>
+				</div>
+				<div class="twitch">
+					<a href="https://www.twitch.tv/meponchannel">
+						<span class="fab fa-twitch"></span><br>
+						<small>Twitch</small>
+					</a>
+				</div>
+			</div>
+	</body>
+</html>
+<!DOCTYPE html>
+<html lang="ru">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta property="og:locale" content="ru_RU"/>
+		<meta property="og:site_name" content="Стример на Twitch"/>
+		<meta property="og:title" content="Mepon" />
+		<meta property="og:type" content="article"/>
+		<meta property="og:url" content="http://mepon.fun" />
+		<meta property="og:image" content="http://mepon.fun/assets/img/icon.png" />
+		<meta property="og:description" content="Оффициальный сайт Twitch стримера Mepon, со всеми основными ссылками." />
+		<meta name="theme-color" content="#7c00db">
+		<title>Mepon | Twitch streamer</title>
+		<link rel="icon" href="assets/img/icon.png">
+		<link rel="stylesheet" href="assets/css/style.css">
+		<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<script src="assets/js/jquery-3.3.1.min.js"></script>
+	</head>
+	<body class="body-main">
+		<div class="container mt-5">
+			<div class="title">
+				<h1><a style="color: #ff0000;" id='online'>◯</a> Mepon channel</h1>
+				<script>
+					var a = ["#d6ac02", "#ff0000", "#00e6ff", "#000dff"];
+					setInterval(function() {
+						document.getElementById('online').style.color = a[Math.floor(Math.random() * Math.floor(4))];
+					},360);
+
+				</script>
+			</div>
+		</div>
+
+			<div class="container text-center w-50 p-4 rounded mt-5">
+				<iframe
+					src="https://player.twitch.tv/?channel=meponchannel&parent=localhost&muted=true"
+					height="500"
+					width="800"
+					frameborder="0"
+					allowfullscreen="true">
+				</iframe>
+			</div>
+
+			<div class="links">
+				<div class="vk">
+					<a href="https://vk.com/mepon" target="_blink">
+						<span class="fab fa-vk"></span><br>
+						<small>VK</small>
+					</a>
+				</div>
+				<div class="discord">
+					<a href="https://discord.gg">
+						<span class="fab fa-discord"></span><br>
+						<small>Discord</small>
+					</a>
+				</div>
+				<div class="donate">
+					<a href="https://donate.stream/mepondonatik">
+						<span class="fas fa-coins"></span><br>
+						<small>Донат</small>
+					</a>
+				</div>
+				<div class="steam">
+					<a href="https://steamcommunity.com/id/mepon">
+						<span class="fab fa-steam"></span><br>
+						<small>Steam</small>
+					</a>
+				</div>
+				<div class="youtube">
+					<a href="https://www.youtube.com/channel/UCTt4todzqkn3vOMFMkHa3Wg">
+						<span class="fab fa-youtube"></span><br>
+						<small>YouTube</small>
+					</a>
+				</div>
+				<div class="twitch">
+					<a href="https://www.twitch.tv/meponchannel">
+						<span class="fab fa-twitch"></span><br>
+						<small>Twitch</small>
+					</a>
+				</div>
+			</div>
+	</body>
+</html>
